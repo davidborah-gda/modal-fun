c59c93e46877eaa2f5ccf1ff5f37b495ca696b9c
David Borah <dborah87@gmail.com>
2018-06-28 20:04:58 -0400

    set eslint and changed the transition of the modal to annimated

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 const modal = {
-    isHidden: true,
-    selectedImage: '',
-    init(){
-        this.cacheDom();
-        this.bindEventListeners();
-        this.render();
-    },
-    cacheDom(){
-        this.modalContainer = document.querySelector('.modal-container');
-        this.closeBtn = document.querySelector('.close');
-        this.modalImage = document.querySelector('.modal-image');
-        this.thumbnails = document.querySelectorAll('.thumbnail');
-    },
-    bindEventListeners(){
-        this.closeBtn.addEventListener('click', this.hideModal.bind(this));
-        this.thumbnails.forEach((thumbnail) => {
-            thumbnail.addEventListener('click', this.showModal.bind(this));
-        });
-    },
-   
-    hideModal(){
-        this.isHidden = true;
-        this.selectedImage = '';
-        this.render();
-    },
+  isHidden: true,
+  selectedImage: '',
+  init() {
+    this.cacheDom();
+    this.bindEventListeners();
+    this.render();
+  },
+  cacheDom() {
+    this.modalContainer = document.querySelector('.modal-container');
+    this.closeBtn = document.querySelector('.close');
+    this.modalImage = document.querySelector('.modal-image');
+    this.thumbnails = document.querySelectorAll('.thumbnail');
+  },
+  bindEventListeners() {
+    this.closeBtn.addEventListener('click', this.hideModal.bind(this));
+    this.thumbnails.forEach((thumbnail) => {
+      thumbnail.addEventListener('click', this.showModal.bind(this));
+    });
+  },
 
-    showModal(evt){
-        this.isHidden = false;
-        this.selectedImage = evt.target.src;
-        this.render();
-    },
+  hideModal() {
+    this.isHidden = true;
+    this.selectedImage = '';
+    this.render();
+  },
 
-    render(){
-        if(this.isHidden){
-            this.modalContainer.classList.add('hidden');
-        }  else {
-            this.modalContainer.classList.remove('hidden');
-            this.modalImage.src = this.selectedImage;
-        }
+  showModal(evt) {
+    this.isHidden = false;
+    this.selectedImage = evt.target.src;
+    this.render();
+  },
+
+  render() {
+    if (this.isHidden) {
+      this.modalContainer.classList.add('hidden');
+    } else {
+      this.modalContainer.classList.remove('hidden');
+      this.modalImage.src = this.selectedImage;
     }
+  },
 };
 modal.init();
 
